ae565959fbebacc755cc68cb5e5509f9e41199d0
Tempetas <tampetus@gmail.com>
2022-12-19 16:51:37 +0100

    Support layered folders

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ cd `dirname $0`
 if [[ -z $1 ]]; then echo 'Please, enter a project name, e.g. ./build.sh ProjectName' && exit 1; fi
 if ! [[ -d ${1%/}/src ]]; then echo 'Directory doesn`t exist, must look like `ProjectName/src/`' && exit 1; fi
 
-mkdir -p ${1%/}/build && javac -d ${1%/}/build -cp sum-jars/\* ${1%/}/src/*.java && java -cp sum-jars/\*:${1%/}/build/ Main
+mkdir -p ${1%/}/build && javac -d ${1%/}/build -cp sum-jars/\* `find ${1%/}/src/ -name \*.java` && java -cp sum-jars/\*:${1%/}/build/ Main
 )
 
